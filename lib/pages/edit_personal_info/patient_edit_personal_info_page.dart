a7eea1065c6aa799894aadd9ce4fefd02fc920a6
rodrigosms17 <rodriguinobo2@gmail.com>
2025-05-21 14:45:49 -0500

    primer push, profile, y pages de profile

---
+++ lib/pages/edit_personal_info/patient_edit_personal_info_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PatientEditPersonalInfoPage extends StatelessWidget {
+  const PatientEditPersonalInfoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Editar datos personales')),
+      body: const Center(
+        child: Text('HOLI'),
+      ),
+    );
+  }
+}
